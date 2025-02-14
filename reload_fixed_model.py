0d43c3bd475ffe800e7315e3d428c293db412d4d
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-02-15 00:03:32 +0530

    delete all

--- reload_fixed_model.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load the modified H5 model (now without batch_shape)
-model = load_model("simpleRNN-IMDB.h5")  # Should work now
-
-# Save a new clean model file
-model.save("simpleRNN-IMDB-v2.h5")
-
-print("✅ New model saved as simpleRNN-IMDB-v2.h5")
